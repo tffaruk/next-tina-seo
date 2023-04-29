c1da1f7fdec24eae69ffca177fa2d44588ce70b8
Omar Faruk <faruk.themefisher@gmail.com>
2023-04-29 16:05:06 +0600

    fixing module js issue

--- dist/NextTinaSeo.js
+++ dist/NextTinaSeo.js
@@ -2,16 +2,15 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 Object.defineProperty(exports, "__esModule", {
-  value: true
+  value: true,
 });
 exports["default"] = void 0;
 var _head = _interopRequireDefault(require("next/head"));
 var _router = require("next/router");
-var _react = _interopRequireDefault(require("react"));
 var _config = _interopRequireDefault(require("../config.json"));
 var _textConverter = require("../textConverter");
-import React from "react";
-var __jsx = React.createElement;
+var _react = _interopRequireDefault(require("react"));
+var __jsx = _react.createElement;
 var NextTinaSeo = function NextTinaSeo(_ref) {
   var title = _ref.title,
     meta_title = _ref.meta_title,
@@ -25,47 +24,78 @@ var NextTinaSeo = function NextTinaSeo(_ref) {
     meta_description = _config$metadata.meta_description;
   var base_url = _config["default"].site.base_url;
   var router = (0, _router.useRouter)();
-  return __jsx(_head["default"], null, __jsx("title", null, (0, _textConverter.plainify)(meta_title ? meta_title : title ? title : _config["default"].site.title)), canonical && __jsx("link", {
-    rel: "canonical",
-    href: canonical,
-    itemProp: "url"
-  }), noindex && __jsx("meta", {
-    name: "robots",
-    content: "noindex,nofollow"
-  }), __jsx("meta", {
-    name: "description",
-    content: (0, _textConverter.plainify)(description ? description : meta_description)
-  }), __jsx("meta", {
-    name: "author",
-    content: meta_author
-  }), __jsx("meta", {
-    property: "og:title",
-    content: title
-  }), __jsx("meta", {
-    property: "og:description",
-    content: (0, _textConverter.plainify)(description ? description : meta_description)
-  }), __jsx("meta", {
-    property: "og:type",
-    content: "website"
-  }), __jsx("meta", {
-    property: "og:url",
-    content: "".concat(base_url, "/").concat(router.asPath.replace("/", ""))
-  }), __jsx("meta", {
-    name: "twitter:title",
-    content: title
-  }), __jsx("meta", {
-    name: "twitter:description",
-    content: (0, _textConverter.plainify)(description ? description : meta_description)
-  }), __jsx("meta", {
-    property: "og:image",
-    content: "".concat(base_url).concat(image ? image : meta_image)
-  }), __jsx("meta", {
-    name: "twitter:image",
-    content: "".concat(base_url).concat(image ? image : meta_image)
-  }), __jsx("meta", {
-    name: "twitter:card",
-    content: "summary_large_image"
-  }));
+  return __jsx(
+    _head["default"],
+    null,
+    __jsx(
+      "title",
+      null,
+      (0, _textConverter.plainify)(
+        meta_title ? meta_title : title ? title : _config["default"].site.title
+      )
+    ),
+    canonical &&
+      __jsx("link", {
+        rel: "canonical",
+        href: canonical,
+        itemProp: "url",
+      }),
+    noindex &&
+      __jsx("meta", {
+        name: "robots",
+        content: "noindex,nofollow",
+      }),
+    __jsx("meta", {
+      name: "description",
+      content: (0, _textConverter.plainify)(
+        description ? description : meta_description
+      ),
+    }),
+    __jsx("meta", {
+      name: "author",
+      content: meta_author,
+    }),
+    __jsx("meta", {
+      property: "og:title",
+      content: title,
+    }),
+    __jsx("meta", {
+      property: "og:description",
+      content: (0, _textConverter.plainify)(
+        description ? description : meta_description
+      ),
+    }),
+    __jsx("meta", {
+      property: "og:type",
+      content: "website",
+    }),
+    __jsx("meta", {
+      property: "og:url",
+      content: "".concat(base_url, "/").concat(router.asPath.replace("/", "")),
+    }),
+    __jsx("meta", {
+      name: "twitter:title",
+      content: title,
+    }),
+    __jsx("meta", {
+      name: "twitter:description",
+      content: (0, _textConverter.plainify)(
+        description ? description : meta_description
+      ),
+    }),
+    __jsx("meta", {
+      property: "og:image",
+      content: "".concat(base_url).concat(image ? image : meta_image),
+    }),
+    __jsx("meta", {
+      name: "twitter:image",
+      content: "".concat(base_url).concat(image ? image : meta_image),
+    }),
+    __jsx("meta", {
+      name: "twitter:card",
+      content: "summary_large_image",
+    })
+  );
 };
 var _default = NextTinaSeo;
 exports["default"] = _default;
